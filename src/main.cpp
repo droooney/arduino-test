5187dd33a60a444dc7ce77330af8e59361f258e4
droooney <d.vor7@mail.ru>
2024-01-24 04:25:48 +0500

    optimizations

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,10 @@
 #define MAX_BRIGHTNESS 7
 #define MAX_LED_VALUE 63
 
+#ifndef DEBUG
+#define DEBUG 1
+#endif
+
 char KEYS_MATRIX[KEYPAD_ROWS_COUNT][KEYPAD_COLS_COUNT] = {
   {'1', '2'},
   {'3', '4'},
@@ -21,16 +25,21 @@ byte currentBrightness = MAX_BRIGHTNESS / 2;
 Keypad customKeypad = Keypad(makeKeymap(KEYS_MATRIX), rowPins, colPins, KEYPAD_ROWS_COUNT, KEYPAD_COLS_COUNT);
 
 void changeBrightness() {
-  Serial.print("Current brightness: ");
+#if DEBUG
+  Serial.print(F("Current brightness: "));
   Serial.print(currentBrightness);
-  Serial.print('/');
+  Serial.print(F("/"));
   Serial.println(MAX_BRIGHTNESS);
+#endif
 
   analogWrite(LED_PIN, (int)((float)currentBrightness * MAX_LED_VALUE / MAX_BRIGHTNESS));
 }
 
 void setup() {
+#if DEBUG
   Serial.begin(9600);
+#endif
+
   pinMode(LED_PIN, OUTPUT);
   changeBrightness();
 }
@@ -38,10 +47,12 @@ void setup() {
 void loop() {
   char key = customKeypad.getKey();
 
+#if DEBUG
   if (key) {
-    Serial.print("Pressed key ");
+    Serial.print(F("Pressed key "));
     Serial.println(key);
   }
+#endif
 
   bool brightnessChanged = false;
 
