fe05b7e916f16e01f425f7d52bc5576b6dc78f78
droooney <d.vor7@mail.ru>
2024-01-25 06:06:39 +0500

    multiple leds

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #define KEYPAD_ROWS_COUNT 2
 #define KEYPAD_COLS_COUNT 2
 
-#define LED_PIN 10
+#define LED_COUNT 4
 #define MAX_BRIGHTNESS 7
 #define MAX_LED_VALUE 63
 
@@ -17,12 +17,35 @@ char KEYS_MATRIX[KEYPAD_ROWS_COUNT][KEYPAD_COLS_COUNT] = {
   {'3', '4'},
 };
 
-byte rowPins[KEYPAD_ROWS_COUNT] = {7, 6};
-byte colPins[KEYPAD_COLS_COUNT] = {8, 9};
+byte rowPins[KEYPAD_ROWS_COUNT] = {4, 2};
+byte colPins[KEYPAD_COLS_COUNT] = {7, 8};
+byte ledPins[LED_COUNT] = {9, 6, 5, 3};
 
 byte currentBrightness = MAX_BRIGHTNESS / 2;
+byte currentLedIndex = 0;
 
 Keypad customKeypad = Keypad(makeKeymap(KEYS_MATRIX), rowPins, colPins, KEYPAD_ROWS_COUNT, KEYPAD_COLS_COUNT);
+
+void setBrightness() {
+  analogWrite(ledPins[currentLedIndex], (int)((float)currentBrightness * MAX_LED_VALUE / MAX_BRIGHTNESS));
+}
+
+void changeLedIndex(byte newIndex) {
+  if (currentBrightness != 0) {
+    analogWrite(ledPins[currentLedIndex], 0);
+  }
+
+  currentLedIndex = newIndex;
+
+#if DEBUG
+  Serial.print(F("Current LED index: "));
+  Serial.println(currentLedIndex);
+#endif
+
+  if (currentBrightness != 0) {
+    setBrightness();
+  }
+}
 
 void changeBrightness() {
 #if DEBUG
@@ -32,7 +55,7 @@ void changeBrightness() {
   Serial.println(MAX_BRIGHTNESS);
 #endif
 
-  analogWrite(LED_PIN, (int)((float)currentBrightness * MAX_LED_VALUE / MAX_BRIGHTNESS));
+  setBrightness();
 }
 
 void setup() {
@@ -40,7 +63,10 @@ void setup() {
   Serial.begin(9600);
 #endif
 
-  pinMode(LED_PIN, OUTPUT);
+  for (auto ledPinIndex : ledPins) {
+    pinMode(ledPins[ledPinIndex], OUTPUT);
+  }
+
   changeBrightness();
 }
 
@@ -55,6 +81,8 @@ void loop() {
 #endif
 
   bool brightnessChanged = false;
+  bool ledChanged = false;
+  byte newLedIndex;
 
   if (key == '1' && currentBrightness > 0) {
     currentBrightness--;
@@ -62,9 +90,17 @@ void loop() {
   } else if (key == '2' && currentBrightness < MAX_BRIGHTNESS) {
     currentBrightness++;
     brightnessChanged = true;
+  } else if (key == '3') {
+    newLedIndex = (currentLedIndex + LED_COUNT - 1) % LED_COUNT;
+    ledChanged = true;
+  } else if (key == '4') {
+    newLedIndex = (currentLedIndex + 1) % LED_COUNT;
+    ledChanged = true;
   }
 
   if (brightnessChanged) {
     changeBrightness();
+  } else if (ledChanged) {
+    changeLedIndex(newLedIndex);
   }
 }
